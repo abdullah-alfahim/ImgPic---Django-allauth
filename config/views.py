bf3fb2ba01dc7404e3d6ec2455dcab126904952e
abdullah-alfahim <abdullahsvoice@gmail.com>
2025-07-08 08:20:33 +0600

    Add Page Done

--- config/views.py
+++ config/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, CreateView
 from .models import Imgs
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -13,3 +14,8 @@ class ImgListView(ListView):
 class ImgCreateView(CreateView):
     model = Imgs
     fields = ['heading', 'image']
+    success_url = reverse_lazy('img_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
--- imgpic/urls.py
+++ imgpic/urls.py
@@ -3,13 +3,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from config.views import ImgListView
+from config.views import ImgListView, ImgCreateView
 import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
     path('', ImgListView.as_view(), name='img_list'),
+    path('add/', ImgCreateView.as_view(), name='img_add'),
 ]
 
 if settings.DEBUG:
